174eeb60ed49f29ce79fdcd10484d60791b67d1c
Jason HUANG <jahuang@e2r3p17.clusters.42paris.fr>
2021-12-12 17:02:19 +0100

    Some minor changes and add Makefile

--- incs/lexer.h
+++ incs/lexer.h
@@ -1,6 +1,31 @@
 #ifndef LEXER_H
 # define LEXER_H
 
+enum char_type {
+	CHAR_GENERAL = -1,
+	CHAR_PIPE = '|',
+	CHAR_GREATER = '>',
+	CHAR_LESSER = '<',
+	CHAR_DOLLER = '$',
+};
 
+enum token_type {
+	TOKEN_WORD = 0,
+	TOKEN_PIPE,
+	TOKEN_GREATER,
+	TOKEN_GREATGREATER,
+	TOKEN_LESSER,
+	TOKEN_LESSLESSER,
+};
+
+typedef struct	s_token {
+	char *value;
+	int	token_type;
+	struct s_token *next;
+}				t_token;
+
+t_token	*ft_init_token(void);
+void	ft_add_token(char *content);
+int		ft_get_type(char *content);
 
 #endif
