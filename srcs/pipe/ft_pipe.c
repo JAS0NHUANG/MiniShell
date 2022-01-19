4e5e601ff5a7f81eeda2a9064c7425f2b6994f45
Antoine an TON THAT <antton-t@e3r5p16.42.fr>
2022-01-19 13:36:29 +0100

    pipe

--- srcs/pipe/ft_pipe.c
+++ srcs/pipe/ft_pipe.c
@@ -6,29 +6,49 @@
 /*   By: antton-t <antton-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 18:02:08 by antton-t          #+#    #+#             */
-/*   Updated: 2022/01/18 14:58:13 by antton-t         ###   ########.fr       */
+/*   Updated: 2022/01/19 12:20:53 by antton-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
+int		ft_count_pipe_tree(t_inary *tree)
+{
+	int	count;
+
+	count = 0;
+	while (tree)
+	{
+		if (tree->type_node == NODE_PIPE)
+			count++;
+		tree = tree->left;
+	}
+	return (count);
+}
+
 void	ft_handle_pipe(t_inary *tree)
 {
 	t_inary	*ptr;
 	int		nb_pipe;
+	pid_t	pid;
 
-	ptr = tree;
-	while (tree && ptr)
+	nb_pipe = ft_count_pipe_tree(tree);
+	while (nb_pipe)
 	{
 		ptr = tree;
-		ptr = ptr->right;
-printf("TREE middle ==>  %s  \n",tree->value[0]);
-/*
-if (ptr->type_node == NODE_LIST)
-printf("TREE RIGHT==>  %s  \n",ptr->t_dir->value);
-else
-*/
-printf("TREE RIGHT==>  %s  \n",ptr->value[0]);
-		tree = tree->left;
+		pid_t = fork;
+		if (pid_t < 0)
+		{
+			printf("FORK fail ERROR\n");
+		}
+		if (pid_t == 0)
+		{
+			printf("in Child\n");
+		}
+		else
+		{
+			printf("in Parent\n");
+		}
+		nb_pipe--;
 	}
 }
