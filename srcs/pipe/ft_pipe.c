cf9f44f5a6dfdf8a056b5bd85c245686c4e1932a
Antoine an TON THAT <antton-t@e2r5p10.clusters.42paris.fr>
2022-01-21 21:29:46 +0100

    ca marche

--- srcs/pipe/ft_pipe.c
+++ srcs/pipe/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: antton-t <antton-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 18:02:08 by antton-t          #+#    #+#             */
-/*   Updated: 2022/01/19 21:48:00 by antton-t         ###   ########.fr       */
+/*   Updated: 2022/01/21 21:28:42 by antton-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,54 @@
 #include <sys/wait.h>
 
 
-void	ft_pipe_child(t_inary *tree, int *fd, char **a)
+void	ft_pipe_child(t_inary *tree, int *fd)
 {
-	if (tree->type_node == NODE_PIPE)
-	{
-		ft_handle_pipe(tree->left);
-	}
-	else
-	{
+
+(void)tree;
+		
 		close(fd[1]);
-		tree->value[0] = ft_strjoin(tree->value[0], "b");
-execve("/bin/ls", a, NULL);
+		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
-	}
+		execlp("wc", "wc", (char *) 0);
+
 }
 
-void	ft_pipe_parent(t_inary *tree, int *fd, char **a)
+void	ft_pipe_parent(t_inary *tree, int *fd)
 {
+//	pid_t	pid;
+
+(void)tree;
 	close(fd[0]);
-	tree->value[0] = ft_strjoin(tree->value[0], "a");
-execve("/bin/ls", a, NULL);
-	close(fd[1]);
+//	pid = fork();
+//	if (pid == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		execlp("ls", "ls", (char *)0);
+	}
 }
 
-int		ft_count_pipe_tree(t_inary *tree)
+void	ft_execve_first_node(t_inary *tree, int *fd)
 {
-	int	count;
-
-	count = 0;
-	while (tree)
-	{
-		if (tree->type_node == NODE_PIPE)
-			count++;
-		tree = tree->left;
-	}
-	return (count);
+(void)tree;
+	close(fd[0]);
+	dup2(fd[1], STDIN_FILENO);
+	close(fd[1]);
+	execlp("ls", "ls", (char *)0);
 }
 
 void	ft_handle_pipe(t_inary *tree)
 {
 	pid_t	pid;
 	int		fd[2];
-	pid_t		count;
-
-//t_inary	*ptr = tree;
-char **a;
-a = calloc(2, sizeof(char *));
-a[0] = calloc(3, sizeof(char));
-a[0][0] = 'l';
-a[0][1] = 's';
+	pid_t	stay;
 
 	if (pipe(fd) == -1)
 		printf("Error pipe creation\n");
 	else
 	{
-		count = fork();
-		if (count == 0)
+		stay = fork();
+		if (stay == 0)
 		{
 			if (tree->left)
 			{
@@ -81,17 +73,21 @@ printf("DANS L ARBRE\n");
 				if (pid == 0)
 				{
 					printf("in Child\n");
-					ft_pipe_child(tree->left, fd, a);
+					ft_pipe_child(tree, fd);
 				}
 				else
 				{
 					printf("in Parent\n");
-					ft_pipe_parent(tree->right, fd, a);
+					ft_pipe_parent(tree->right, fd);
 				}
 			}
+			else
+				ft_execve_first_node(tree, fd);
 		}
-//		printf("Value final => %s\n",ptr->right->value[0]);
+//printf("Value final => %s\n",ptr->right->value[0]);
 //execve("/bin/ls", a, NULL);
 	}
 	waitpid(-1, NULL, 0);
+	close(fd[0]);
+	close(fd[1]);
 }
